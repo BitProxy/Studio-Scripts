e5416e2d84682ce078daebae395f864d5b8c2eff
BitProxy <129908747+BitProxy@users.noreply.github.com>
2023-04-21 08:56:03 -0700

    Create create.lua

---
+++ create.lua
@@ -0,0 +1,55 @@
+-- Shortcuts for BitProxy create
+
+local Creation = {};
+local Service = (game and game.GetService) or nil;
+local Create = (Instance and Instance.new) or nil;
+
+Creation.Enabled = true;
+
+if not Create or not Service then
+  error("Something went wrong!", 7);
+end;
+
+function setIndex(instance, index, value)
+  instance[index] = value;
+end;
+
+function creation(className, properties)
+  local class = Create(className);
+  for index, value in pairs(properties) do
+    pcall(setIndex, class, index, value);
+  end;
+  return class;
+end;
+
+function Creation.toggle()
+  Creation.Enabled = not Creation.Enabled;
+end;
+
+function Creation.createInstance(...)
+  if Creation.Enabled == false then return end;
+  local success, data = pcall(creation, ...);
+  return (success and data) or nil;
+end;
+
+function Creation.RemoveInstance(object)
+  if Creation.Enabled == false then return end;
+  pcall(object.Destroy, object);
+end;
+
+function Creation.createRemote(remoteType, remoteFunction, ...)
+  if Creation.Enabled == false then return end;
+  local remote = "RemoteFunction";
+  if not remoteType or remoteType == 1 then
+    remote = "RemoteEvent";
+  end;
+  local Class = Creation.createInstance(remote, {...});
+  if remote == "RemoteEvent" then
+    remote.OnServerEvent:Connect(remoteFunction);
+  else
+    remote.OnServerInvoke = remoteFunction;
+  end;
+  return Class;
+end;
+
+return Creation;
