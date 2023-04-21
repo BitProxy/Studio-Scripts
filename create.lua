163675de2f861dc09f6407ae001c365ac97b038b
BitProxy <129908747+BitProxy@users.noreply.github.com>
2023-04-21 15:34:11 -0700

    Update create.lua

--- create.lua
+++ create.lua
@@ -7,49 +7,49 @@ local Create = (Instance and Instance.new) or nil;
 Creation.Enabled = true;
 
 if not Create or not Service then
-  error("Something went wrong!", 7);
+	error("Something went wrong!", 7);
 end;
 
 function setIndex(instance, index, value)
-  instance[index] = value;
+	instance[index] = value;
 end;
 
 function creation(className, properties)
-  local class = Create(className);
-  for index, value in pairs(properties) do
-    pcall(setIndex, class, index, value);
-  end;
-  return class;
+	local class = Create(className);
+	for index, value in pairs(properties) do
+		pcall(setIndex, class, index, value);
+	end;
+	return class;
 end;
 
 function Creation.toggle()
-  Creation.Enabled = not Creation.Enabled;
+	Creation.Enabled = not Creation.Enabled;
 end;
 
 function Creation.createInstance(...)
-  if Creation.Enabled == false then return end;
-  local success, data = pcall(creation, ...);
-  return (success and data) or nil;
+	if Creation.Enabled == false then return end;
+	local success, data = pcall(creation, ...);
+	return (success and data) or nil;
 end;
 
 function Creation.RemoveInstance(object)
-  if Creation.Enabled == false then return end;
-  pcall(object.Destroy, object);
+	if Creation.Enabled == false then return end;
+	pcall(object.Destroy, object);
 end;
 
-function Creation.createRemote(remoteType, remoteFunction, ...)
-  if Creation.Enabled == false then return end;
-  local remote = "RemoteFunction";
-  if not remoteType or remoteType == 1 then
-    remote = "RemoteEvent";
-  end;
-  local Class = Creation.createInstance(remote, {...});
-  if remote == "RemoteEvent" then
-    Class.OnServerEvent:Connect(remoteFunction);
-  else
-    Class.OnServerInvoke = remoteFunction;
-  end;
-  return Class;
+function Creation.createRemote(remoteType, remoteFunction, properties)
+	if Creation.Enabled == false then return end;
+	local remote = "RemoteFunction";
+	if not remoteType or remoteType == 1 then
+		remote = "RemoteEvent";
+	end;
+	local Class = Creation.createInstance(remote, properties or {});
+	if remote == "RemoteEvent" then
+		Class.OnServerEvent:Connect(remoteFunction);
+	else
+		Class.OnServerInvoke = remoteFunction;
+	end;
+	return Class;
 end;
 
 return Creation;
